5eecbbcb4d30059f59cc2aced33ec7b654c762a4
rajan88lal88 <rajan88lal88@gmail.com>
2020-02-28 12:47:47 +0530

    Lecture 4

---
+++ Lecture-5/Activity/tpp.js
@@ -0,0 +1,29 @@
+let input=process.argv[2];
+
+let viewFile=require("./commands/view");
+let treefyFile=require("./commands/treefy");
+let untreefyFile=require("./commands/untreefy");
+let monitorFile=require("./commands/monitor");
+let helpFile=require("./commands/help");
+
+switch(input)
+{
+    case "view":
+            viewFile.view(process.argv[3],process.argv[4]);
+            break;
+    case "untreefy":
+            untreefyFile.untreefy(process.argv[3],process.argv[4]);
+            break;
+    case "treefy":
+            treefyFile.treefy(process.argv[3],process.argv[4]);
+            break;
+    case "monitor":
+            monitorFile.monitor(process.argv[3],process.argv[4]);
+            break;
+    case "help":
+            helpFile.help();
+            break;
+    default:
+            console.log("Wrong Command");
+}    
+
