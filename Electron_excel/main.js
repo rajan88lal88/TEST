e89cda33544622c71de2740e04383ec2c9f0fffa
rajan88lal88 <rajan88lal88@gmail.com>
2020-08-05 19:07:39 +0530

    vscode

---
+++ Electron_excel/main.js
@@ -0,0 +1,43 @@
+const electron=require("electron");
+const app=electron.app;
+const ejs=require("ejs-electron");
+ejs.data({
+  "title":"my excel",
+  "rows":100,
+  "cols":26
+})
+function createWindow () {
+  // Create the browser window.
+  const win = new electron.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.ejs').then(function(){
+      win.maximize();
+      win.show();
+      win.webContents.openDevTools();
+  })
+}
+
+app.whenReady().then(createWindow);
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
